d66d38574f4b91f45b769dcb1ad97b4f79b99e08
KarinaChiste <163331375+KarinaChiste@users.noreply.github.com>
2025-02-03 15:35:38 -0700

    Part 1 Completed

---
+++ Lab3/Cat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public class Cat : Animal
+    {
+        public Cat(string name, string colour, int age): base(name, colour, age) { }
+        public override void Eat()
+        {
+            Console.WriteLine("Cats eat mice.");
+        }
+    }
+}
--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// PART 1
+
+using Lab3;
+
+Console.WriteLine("Enter Dog Name: ");
+string dogName = Console.ReadLine();
+
+Dog dog = new Dog(dogName, "Brown", 5);
+
+Console.WriteLine($"\nDog Name: {dog.Name} \nColour:{dog.Colour} \nAge:{dog.Age}");
+dog.Eat();
+
+Console.WriteLine("\nEnter Cat Name: ");
+string catName = Console.ReadLine();
+
+Cat cat = new Cat(catName, "White", 10);
+
+Console.WriteLine($"\nCat Name: {cat.Name} \nColour:{cat.Colour} \nAge:{cat.Age}");
+cat.Eat();
